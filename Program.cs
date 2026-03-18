1df2c336702bb6b0956c278d6b02c653013b848a
elefuckinmixtape <elesmixtape@protonmail.com>
2026-03-18 16:13:10 -0600

    new print function :>

--- Program.cs
+++ Program.cs
@@ -14,8 +14,9 @@ namespace BMICALCULATOR
     }
     internal class Program
     {
-        public static void Print(string output)
+        public static void Print(int delay, string output)
         {
+            Thread.Sleep(TimeSpan.FromSeconds(delay));
             if (!output.Contains("Loading", StringComparison.OrdinalIgnoreCase))
             { // output not Loading
                 Console.WriteLine($"> {output}");
@@ -32,13 +33,13 @@ namespace BMICALCULATOR
                 int loadTicks = 9; // Amount of times it will do a load cycle (every 4 is a full animated cycle)
                 int currentDotTicks = 0; // Amount of current "." on loading text
                 string currentText = $"Loading"; // Current text to display
-                Print($"{currentText}");
+                Print(0, $"{currentText}");
                 for (int i = 0; i < loadTicks; i++)
                 {
                     if (currentDotTicks < 4)
                     {
                         Console.SetCursorPosition(0, Console.CursorTop - 1);
-                        Print($"{currentText}");
+                        Print(0, $"{currentText}");
                         currentText = $"{currentText}.";
                         currentDotTicks++;
                         Thread.Sleep(TimeSpan.FromSeconds(1));
@@ -48,29 +49,28 @@ namespace BMICALCULATOR
                         Console.SetCursorPosition(13, Console.CursorTop - 1); Console.WriteLine($"\b\b\b\b     ");
                         Console.SetCursorPosition(0, Console.CursorTop - 1);
                         currentText = $"Loading";
-                        Print($"{currentText}");
+                        Print(0, $"{currentText}");
 
                         currentDotTicks = 0;
                     }
                 }
-                Thread.Sleep(TimeSpan.FromSeconds(1));
-                Print($"LOADING COMPLETE!");
+                Print(1, $"LOADING COMPLETE!");
             }
             
             // Start of "Main"
-            Print($"BMI CALCULATOR INITIALIZING..{Environment.NewLine}>       PLEASE WAIT...");
+            Print(0, $"BMI CALCULATOR INITIALIZING..{Environment.NewLine}>       PLEASE WAIT...");
             Thread.Sleep(TimeSpan.FromSeconds(.5)); FakeLoad();
             // Continue "Main" method
-            Thread.Sleep(TimeSpan.FromSeconds(2)); Print($"Welcome to the BMI Calculator!");
-            Thread.Sleep(TimeSpan.FromSeconds(1)); Print($"This program will calculate your average BMI.");
-            Thread.Sleep(TimeSpan.FromSeconds(1)); Print($"To start off, please input your height..");
+            Print(2, $"Welcome to the BMI Calculator!");
+            Print(1, $"This program will calculate your average BMI.");
+            Print(1, $"To start off, please input your height..");
             Console.Write($"> "); while(!double.TryParse(Console.ReadLine(), out doubleHeight))
             {
-                Print($" > ERROR! input is not a double value please input something like this: 180 or 150.6.");
+                Print(0, $" > ERROR! input is not a double value please input something like this: 180 or 150.6.");
                 Console.Write($"> ");
             }
-            Thread.Sleep(TimeSpan.FromSeconds(1)); Print($"Perfect! your 'Height' is : {doubleHeight.ToString()}?");
-            Thread.Sleep(TimeSpan.FromSeconds(1)); Print($"If you have made a mistake please input 'Yes'. If not please input 'No'.");
+            Print(1, $"Perfect! your 'Height' is : {doubleHeight.ToString()}?");
+            Print(1, $"If you have made a mistake please input 'Yes'. If not please input 'No'.");
             Console.Write($"> "); 
            
 
