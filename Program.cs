6b82ff8d69b17b9e90f9095e4fd97bcc53efc18d
Max <elesmixtape@protonmail.com>
2026-04-10 12:20:08 -0600

    reworking everything fuck my stupid chud life!

--- Program.cs
+++ Program.cs
@@ -9,71 +9,28 @@ namespace BMICALCULATOR
     public static class Variables
     // Variables class/storage
     {
-        public static string currentInput;
-        public static double doubleHeight;
-        public static double doubleWeight;
+        public static string currentInput; 
+        public static double doubleHeight; // Height value
+        public static double doubleWeight; // Weight value
     }
     internal class Program
     {
-        static void GetInput()
+        static void GetInput(Type inputType, List<string> allowedInputs)
         {
-            static void ValidateInput(string input)
+            static void ValidateInput(string input, Type inputType, List<string> allowedInputs)
             {
-                 
+                object temp = Convert.ChangeType(input, inputType);
+                if ()
+                
             }
-            string tempInput = Console.ReadLine();
-
-        }
-        static void Print(int delay, string output)
-        {
-            Thread.Sleep(TimeSpan.FromSeconds(delay));
-            if (!output.Contains("Loading", StringComparison.OrdinalIgnoreCase))
-            { // output not Loading
-                Console.WriteLine($"> {output}");
-            } else 
-            { // output = "Loading"
-                Console.WriteLine($" > {output}"); 
-            };
+            string tempInput = Console.ReadLine(); // Get input
+            
         }
         // Main method
         static void Main(string[] args)
         {
-            static void FakeLoad()
-            {
-                int loadTicks = 9; // Amount of times it will do a load cycle (every 4 is a full animated cycle)
-                int currentDotTicks = 0; // Amount of current "." on loading text
-                string currentText = $"Loading"; // Current text to display
-                Print(0, $"{currentText}");
-                for (int i = 0; i < loadTicks; i++)
-                {
-                    if (currentDotTicks < 4)
-                    {
-                        Console.SetCursorPosition(0, Console.CursorTop - 1);
-                        currentText = $"{currentText}.";
-                        Print(0, $"{currentText}");
-                        currentDotTicks++;
-                        Thread.Sleep(TimeSpan.FromSeconds(1));
-                    }
-                    else
-                    {
-                        Console.SetCursorPosition(13, Console.CursorTop - 1); Console.WriteLine($"\b\b\b\b     ");
-                        Console.SetCursorPosition(0, Console.CursorTop - 1);
-                        currentText = $"Loading";
-                        Print(0, $"{currentText}");
-
-                        currentDotTicks = 0;
-                    }
-                }
-                Print(1, $"LOADING COMPLETE!");
-            }
-            
             // Start of "Main"
-            Print(0, $"BMI CALCULATOR INITIALIZING..{Environment.NewLine}>       PLEASE WAIT...");
-            Thread.Sleep(TimeSpan.FromSeconds(.5)); FakeLoad();
-            // Continue "Main" method
-            Print(2, $"Welcome to the BMI Calculator!");
-            Print(1, $"This program will calculate your average BMI.");
-            Print(1, $"To start off, please input your height in meters..");
+            GetInput(typeof(double), null); // Get "height" in meters
             
             
 
