552ac6a5fb0d661a2ee417aa4192d49221f0aaf9
elefuckinmixtape <elesmixtape@protonmail.com>
2026-04-14 11:31:40 -0600

    mrowmrow

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.Design;
 using System.Threading;
 using System.Threading.Tasks;
 using static BMICALCULATOR.Variables;
+using static BMICALCULATOR.inputHandler;
 
 namespace BMICALCULATOR
 {
@@ -15,26 +16,10 @@ namespace BMICALCULATOR
     }
     internal class Program
     {
-        static void GetInput(Type inputType, List<string> allowedInputs)
-        {
-            static void ValidateInput(string input, Type inputType, List<string> allowedInputs)
-            {
-                object temp = Convert.ChangeType(input, inputType);
-                if ()
-                
-            }
-            string tempInput = Console.ReadLine(); // Get input
-            
-        }
         // Main method
         static void Main(string[] args)
         {
-            // Start of "Main"
-            GetInput(typeof(double), null); // Get "height" in meters
-            
-            
-
-           
+            // Start of "Main"meoem9ew
 
         }
     }
---
+++ inputHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BMICALCULATOR
+{
+    public class inputHandler
+    {
+ 
+        private void ValidateInput()
+        {
+            
+        }
+        public void GetInput()
+        {
+            
+        }
+
+        
+    }
+}
