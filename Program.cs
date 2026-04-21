08179173c93406ab5736a949bb69b90f6416ab1e
elefuckinmixtape <elesmixtape@protonmail.com>
2026-04-21 11:37:40 -0600

    yehdghshd

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace BMICALCULATOR
             Console.WriteLine($"===== BMI Calculator ====="); Thread.Sleep(TimeSpan.FromSeconds(1.5));
             Console.WriteLine($"This program will calculate your Body Mass Index (BMI)"); Thread.Sleep(TimeSpan.FromSeconds(1));
             Console.WriteLine($"Please enter your height in meters, Ex: '1.75'");
-            currentInput = inputHandler.GetInput();
+            currentInput = inputHandler.GetInput($"string");
             Console.WriteLine($"Perfect! Your height is {currentInput}? Please input 'Yes' or 'No'");
 
             Console.WriteLine($"Next, please enter your weight in kilograms, Ex: '70.5'");
