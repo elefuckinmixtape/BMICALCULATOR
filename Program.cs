0f2ffdb16ec90af712fa0bf72914c3a859b2b3c7
Max <elesmixtape@protonmail.com>
2026-05-08 11:48:07 -0600

    "PRINT" Method added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
 using System.Runtime.CompilerServices;
+
+using static BMICALCULATOR.Program;
 
 namespace BMICALCULATOR
 {
@@ -11,24 +14,34 @@ namespace BMICALCULATOR
         private double D_Height;
         private double D_Weight;
 
-        void PRINT(string toPrint, float delay)
+        static void PRINT(string toPrint, float delay)
         {
             string P_PrintMessage;
             float P_Delay;
 
             if (!string.IsNullOrEmpty(toPrint))
             {
-
+                P_PrintMessage = toPrint;
+                if (delay !< 0f)
+                {
+                    P_Delay = delay;
+                }
+                else
+                {
+                    P_Delay = 0f;
+                }
+                Thread.Sleep(TimeSpan.FromSeconds(P_Delay));
+                Console.WriteLine(P_PrintMessage);
             }
             else
             {
-
+                Console.WriteLine($"ERROR PRINT MESSAGE IS INVALID! Please breathe deeply from the toilet bowl.");
             }
         }
         static void Main(string[] args)
-        {   
+        {
             // Start :>
-            
+            PRINT($"MEWO", 1);
         }
     }
 }
