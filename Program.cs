43b8cbe57419491c571b96ee1f5b15abd7ec67e7
Max <elesmixtape@protonmail.com>
2026-05-20 12:14:29 -0600

    FINISHED FUCKED MY BOY UP BUT HES STILL THERE

--- Program.cs
+++ Program.cs
@@ -1,111 +1,40 @@
 ﻿using System;
-using System.Threading;
 using System.Runtime.CompilerServices;
-
-using static BMICALCULATOR.Program;
 
 namespace BMICALCULATOR
 {
     internal class Program
     {
-        // TempVars
-        static string currentInput;
-
-        static double D_Height;
-        static double D_Weight;
-
-        static void PRINT(string toPrint, float delay)
-        {
-            // TempVars
-            string P_PrintMessage;
-            float P_Delay;
-
-            if (!string.IsNullOrEmpty(toPrint))
-            {
-                P_PrintMessage = toPrint;
-                if (delay !< 0f)
-                {
-                    P_Delay = delay;
-                }
-                else
-                {
-                    P_Delay = 0f;
-                }
-                Thread.Sleep(TimeSpan.FromSeconds(P_Delay));
-                Console.WriteLine(P_PrintMessage);
-            }
-            else
-            {
-                Console.WriteLine($"ERROR PRINT MESSAGE IS INVALID! Please breathe deeply from the toilet bowl.");
-            }
-        }
-        static bool VALIDATE_INPUT(string input) 
-        {
-            // TempVars
-            double D_TryParse;
-
-            if (double.TryParse(input, out D_TryParse))
-            {
-                // "R_Input" is a double
-                return true;
-
-            }
-            else if (!string.IsNullOrEmpty(input))
-            {
-                return true;
-            }
-            return false;
-        }
-        static string REG_INPUT()
-        {
-            // TempVars
-            string R_Input;
-            bool IsValid;
-
-            R_Input = Console.ReadLine();
-            IsValid = VALIDATE_INPUT(R_Input);
-            if (!IsValid) { return null; }
-            return R_Input;
-
-        }
-        
-        string GETINPUT()
-        {
-            // TempVars
-            string tmp_currentInput;
-
-            double D_TryParse;
-
-            tmp_currentInput = Console.ReadLine();
-            if (double.TryParse(tmp_currentInput, out D_TryParse))
-            {
-
-            }
-            else if (!string.IsNullOrEmpty(tmp_currentInput))
-            {
-
-            }
-        }
         static void Main(string[] args)
         {
+            // Vars
+            string currentInput;
+
+            double D_Height;
+            double D_Weight;
             double F_TotalBMI;
 
             // Start :>
-            PRINT($"===== BMI Calculator =====", 0f);
-            PRINT($"This program will calculate your Body Mass Index (BMI)!", 3f);
+            Console.WriteLine($"===== BMI Calculator =====");
+            Console.WriteLine($"This program will calculate your Body Mass Index (BMI)!");
             Console.WriteLine();
-            PRINT($"Please enter your 'height' in meters: EX 1.75 ", 1f);
-            D_Height = double.Parse(REG_INPUT());
+            Console.WriteLine($"Please enter your 'height' in meters: EX 1.75");
             Console.WriteLine();
-            PRINT($"Perfect!", 1f);
-            PRINT($"Now, please enter your 'weight' in kilograms: EX 70.5", 3f);
-            D_Weight = double.Parse(REG_INPUT());
+            currentInput = Console.ReadLine();
             Console.WriteLine();
-            PRINT($"Perfect! The height you entered is ({D_Height}) in meters and the weight you entered is ({D_Weight}) in kilograms.", 3f);
-            PRINT($"Please wait a moment while your 'BMI' is being calculated.", 1f);
+            D_Height = double.Parse(currentInput);
+            Console.WriteLine($"Perfect!");
+            Console.WriteLine($"Now, please enter your 'weight' in kilograms: EX 70.5");
+            Console.WriteLine();
+            currentInput = Console.ReadLine();
+            Console.WriteLine();
+            D_Weight = double.Parse(currentInput);
+            Console.WriteLine($"Perfect! The height you entered is ({D_Height}) in meters and the weight you entered is ({D_Weight}) in kilograms.");
+            Console.WriteLine($"Please wait a moment while your 'BMI' is being calculated!");
             F_TotalBMI = D_Weight / Math.Pow(D_Height, 2f);
-            PRINT($"Your total 'BMI' is : {F_TotalBMI}", 2f);
-
+            Console.WriteLine($"Your total 'BMI' is : {F_TotalBMI}");
+            Console.WriteLine();
+            Console.WriteLine($"Program is now closing.");
         }
     }
 }
